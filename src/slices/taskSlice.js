bb50d89748977fff9b142eccf72dfe67a7ebdadc
Kowthaman <kandasamykowtham125@gmail.com>
2024-11-09 12:27:54 +0530

    Store and slice added

---
+++ src/slices/taskSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  taskList: [],
+  selectedTask: {}
+}
+
+export const taskSlice = createSlice({
+  name: 'taskSlice',
+  initialState,
+  reducers: {
+    addTaskToList: (state,action) => {
+        const id = Math.random() * 100
+        let task = {...action.payload,id}
+        state.taskList.push(task)
+    },
+    removeTaskFromList: (state,action) => {
+        state.taskList = state.taskList.filter((task) => task.id !== action.payload.id)
+    },
+    updateTaskFromList: (state,action) => {
+        state.taskList = state.taskList.map((task) => task.id === action.payload.id ? action.payload : task)
+    },
+    setSelectedTask : (state,action) => {
+        state.selectedTask = action.payload
+    }
+  },
+})
+
+export const { addTaskToList, removeTaskFromList, updateTaskFromList, setSelectedTask } = taskSlice.actions
+
+export default taskSlice.reducer
